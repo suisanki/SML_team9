2f53e801b3fd4c31358a562ba1570f99e8564c25
Spencer Michalke <spencer.j.michalke@utexas.edu>
2024-04-05 11:02:22 -0500

    Update modelSelection.py

---
+++ modelSelection.py
@@ -0,0 +1,7 @@
+'''
+Replace this with updated code.
+
+In the meantime, I created a Google Collab for now to get started. 
+https://colab.research.google.com/drive/1mF4Io3-N3z9FKOJ5jtItIdbRBEXw4wBC?usp=sharing
+~ Spencer
+'''
